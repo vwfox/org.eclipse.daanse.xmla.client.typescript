267713011a8d89a97ab1e53fa54dd7059eb7e002
vwfox <markus.hochstein@software-hochstein.de>
2024-10-01 09:18:25 +0200

    viewmode by config and url

---
+++ public/config/settings.js
@@ -0,0 +1,13 @@
+(function(window) {
+    window.__env = window.__env || {};
+
+    window.__env.settings = {
+        "initWithConfigurationURI": {
+            enabled:false,
+            url:""
+        },
+        "viewmodeByDefault":false,
+
+    };
+
+})(this);
